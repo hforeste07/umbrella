8528743f1b8f3960d565b7fa10498d8c4e2720b8
Heather Forester <hforeste07@gmail.com>
2025-03-11 20:27:38 +0000

    copied and pasted

--- umbrella.rb
+++ umbrella.rb
@@ -26,5 +26,61 @@ loc = geo.fetch("location")
 latitude = loc.fetch("lat")
 longitude = loc.fetch("lng")
 
-pp latitude
-pp longitude
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_key}/#{latitude},#{longitude}"
+
+# p "Getting weather from:"
+# p pirate_weather_url
+
+raw_pirate_weather_data = HTTP.get(pirate_weather_url)
+
+parsed_pirate_weather_data = JSON.parse(raw_pirate_weather_data)
+
+currently_hash = parsed_pirate_weather_data.fetch("currently")
+
+current_temp = currently_hash.fetch("temperature")
+
+puts "It is currently #{current_temp}°F."
+
+# Some locations around the world do not come with minutely data.
+minutely_hash = parsed_pirate_weather_data.fetch("minutely", false)
+
+if minutely_hash
+  next_hour_summary = minutely_hash.fetch("summary")
+
+  puts "Next hour: #{next_hour_summary}"
+end
+
+hourly_hash = parsed_pirate_weather_data.fetch("hourly")
+
+hourly_data_array = hourly_hash.fetch("data")
+
+next_twelve_hours = hourly_data_array[1..12]
+
+precip_prob_threshold = 0.10
+
+any_precipitation = false
+
+next_twelve_hours.each do |hour_hash|
+  precip_prob = hour_hash.fetch("precipProbability")
+
+  if precip_prob > precip_prob_threshold
+    any_precipitation = true
+
+    precip_time = Time.at(hour_hash.fetch("time"))
+
+    seconds_from_now = precip_time - Time.now
+
+    hours_from_now = seconds_from_now / 60 / 60
+
+    puts "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of precipitation."
+  end
+end
+
+if any_precipitation == true
+  puts "You might want to take an umbrella!"
+else
+  puts "You probably won't need an umbrella."
+end
+
