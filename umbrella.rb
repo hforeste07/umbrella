5d1466def0fdefed430873c5e4f2f20cb2373094
Heather Forester <hforeste07@gmail.com>
2025-03-11 20:25:53 +0000

    got the locations to work

--- umbrella.rb
+++ umbrella.rb
@@ -2,3 +2,29 @@
 require "http"
 require "json"
 require "dotenv/load"
+
+
+pp "where are you located?"
+
+user_location = gets.chomp
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+first_result = results.at(0)
+geo = first_result.fetch("geometry")
+loc = geo.fetch("location")
+
+latitude = loc.fetch("lat")
+longitude = loc.fetch("lng")
+
+pp latitude
+pp longitude
